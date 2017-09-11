fcbe7aa1aef0ad78d8b480d39fa52b793e4c1155
Seatis <atikezer@gmail.com>
2017-09-11 11:17:54 +0200

    day-01(feat): python exercises are done until 05

---
+++ week-02/day-01/hello_others.py
@@ -0,0 +1,11 @@
+# Greet 3 of your classmates with this program, in three separate lines
+# like:
+#
+# Hello, Esther!
+# Hello, Mary!
+# Hello, Joe!
+st1 = "Hello, Zsuzsi!\n"
+st2 = "Hello, Atikam!\n"
+st3 = "Hello, Peti!"
+
+print (st1 + st2 + st3)
---
+++ week-02/day-01/introduce_yourself.py
@@ -0,0 +1,14 @@
+# Write a program that prints a few details to the terminal window about you
+# It should print each detail to a new line.
+#  - Your name
+#  - Your age
+#  - Your height in meters (as a decimal fraction)
+#
+#  Example output:
+#  John Doe
+#  31
+#  1.87
+st1 = "Attila Kezer\n"
+st2 = 31
+st3 = 1.87
+print (st1 + str(st2) + "\n" + str(st3))

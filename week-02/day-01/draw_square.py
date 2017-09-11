036c1b267e752eb7da039bc6cd1f36ba6476420a
Seatis <atikezer@gmail.com>
2017-09-11 17:37:47 +0200

    day-01(feat): python exercises are done + added draw_square.py

--- week-02/day-01/draw_square.py
+++ week-02/day-01/draw_square.py
@@ -10,3 +10,18 @@
 # %%%%%
 #
 # The square should have as many lines as the number was
+lines = int(input("Number of lines: "))
+puffer = ""
+
+for k in range(0, lines):
+    for i in range(0, lines):
+        if k == 0 or k == lines - 1:
+            puffer += "%"
+        else:
+            if i == 0 or i == lines - 1:
+                puffer += "%"
+            else:
+                puffer += " "
+           
+    print (puffer + "\n")
+    puffer = ""

dbf3d057ecb551a830c79ca47a400b6d3c6a0688
Seatis <atikezer@gmail.com>
2017-09-11 16:19:17 +0200

    day-01(feat): python exercises are done until 32 - diagonal is done finally, hurray!

--- week-02/day-01/draw_diagonal.py
+++ week-02/day-01/draw_diagonal.py
@@ -10,3 +10,20 @@
 # %%%%%
 #
 # The square should have as many lines as the number was
+lines = int(input("Number of lines: "))
+puffer = ""
+
+for k in range(0, lines):
+    for i in range(0, lines):
+        if k == 0 or k == lines - 1:
+            puffer += "%"
+        else:
+            puffer = "%"
+            for l in range(1, lines - 1):
+                if l == k:
+                    puffer += "%"
+                else:
+                    puffer += " "
+            puffer += "%"
+    print (puffer + "\n")
+    puffer = ""

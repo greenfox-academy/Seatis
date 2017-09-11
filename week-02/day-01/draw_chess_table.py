b321c770beed81d643b1fd59327766f449d25cba
Seatis <atikezer@gmail.com>
2017-09-11 16:54:23 +0200

    day-01(feat): python exercises are done - finished

--- week-02/day-01/draw_chess_table.py
+++ week-02/day-01/draw_chess_table.py
@@ -9,3 +9,18 @@
 # % % % % 
 #  % % % %
 #
+puffer = ""
+for i in range(0, 8):
+    for j in range(0, 8):
+        if i % 2 == 0:
+            if j % 2 == 0:
+                puffer += "%"
+            else:
+                puffer += " "
+        else:
+            if j % 2 == 0:
+                puffer += " "
+            else:
+                puffer += "%"
+    print (puffer + "\n")
+    puffer = ""

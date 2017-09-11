7fdc64e0fe84f0ea9459411d1971c6cb68da3b38
Seatis <atikezer@gmail.com>
2017-09-11 16:30:21 +0200

    day-01(feat): python exercises are done until 33

--- week-02/day-01/guess_the_number.py
+++ week-02/day-01/guess_the_number.py
@@ -5,3 +5,13 @@
 # The stored number is higher
 # The stried number is lower
 # You found the number: 8
+guessnum = 17
+usernum = 0
+while usernum != guessnum:
+    usernum = int(input("Guess the number: "))
+    if usernum > guessnum:
+        print ("The The stried number is lower")
+    elif usernum < guessnum:
+        print ("The stored number is higher")
+    else:
+        print("You found the number: " + str(guessnum))

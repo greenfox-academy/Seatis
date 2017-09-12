15925d60804e5f4ea9fc1e96c8fac61671c7cfde
Seatis <atikezer@gmail.com>
2017-09-12 17:01:10 +0200

    day-03(feat): it is done until bank_transfer

--- week-02/day-02/append_a_list.py
+++ week-02/day-02/append_a_list.py
@@ -3,9 +3,9 @@
 # - Add all elements an `"a"` at the end
 nimals = ["kuty", "macsk", "cic"]
 j = 0
-for i in nimals:
-    i += "a"
-    nimals[j] = i
-    print(i)
+for name in nimals:
+    name += "a"
+    nimals[j] = name
+    print(name)
     j += 1
 print(nimals)

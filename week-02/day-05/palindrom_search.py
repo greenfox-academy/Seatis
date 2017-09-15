660d3361df6f60ad7b8c7f44b5938e47004337f6
Seatis <atikezer@gmail.com>
2017-09-15 16:25:26 +0200

    styleguide-fixed(day-05): palindrom_search.py

--- week-02/day-05/palindrom_search.py
+++ week-02/day-05/palindrom_search.py
@@ -1,21 +1,21 @@
 # mystring = input("Type something pls: ")
 
-def palindrom_finder(finder_string):        # check is it a palindrom
+def palindrome_finder(finder_string):                           # check is it a palindrome
     return finder_string == ''.join(reversed(finder_string))
 
-def palindrom_core(string):
-    palindrom_string = []
-    for j in range(0,len(string)-2):        # length of the string -2 beacuse we need at least 3 char words
-        for k in range(j+3,len(string)+1):  # add one more character, until the end of the string
+def palindrome_core(string):
+    palindrome_string = []
+    for j in range(0,len(string)-2):                            # length of the string -2 beacuse we need at least 3 char words
+        for k in range(j+3,len(string)+1):                      # add one more character, until the end of the string
             puffer = ""
-            for i in range(j,k):            # make a substring until k index, k is at least 3 char long, because, we need at least 3 char words
-                puffer += (string[i])       # put substring in puffer 
-            if palindrom_finder(puffer):    # check if puffer is a palindrom
-                palindrom_string.append(puffer)
-    return palindrom_string
+            for i in range(j,k):                                # make a substring until k index, k is at least 3 char long, because, we need at least 3 char words
+                puffer += (string[i])                           # put substring in puffer 
+            if palindrome_finder(puffer):                       # check if puffer is a palindrome
+                palindrome_string.append(puffer)                # if it is a palindrome, put it in list
+    return palindrome_string
 
 def main():
     mystring = "dog goat dad duck doodle never" 
-    print(palindrom_core(mystring))
+    print(palindrome_core(mystring))
 
 main()

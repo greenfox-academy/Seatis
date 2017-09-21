4c8b8e828c2372f94b13d5290250bef980839b2c
Seatis <atikezer@gmail.com>
2017-09-21 17:28:04 +0200

    style(day-04): styleguide fixed in carpet.py

--- week-03/day-04/carpet.py
+++ week-03/day-04/carpet.py
@@ -6,7 +6,7 @@ canvas = Canvas(root, width='600', height='600')
 canvas.pack()
 
 def magic(x, y, a):
-    lime_box = canvas.create_rectangle(x, y, x+a, y+a, fill='yellow')
+    # lime_box = canvas.create_rectangle(x, y, x+a, y+a, fill='yellow')
     line = canvas.create_line(x, y+a/3, x+a, y+a/3, fill='black')
     line = canvas.create_line(x, y+2*a/3, x+a, y+2*a/3, fill='black')
     line = canvas.create_line(x+a/3, y, x+a/3, y+a, fill='black')
@@ -14,8 +14,9 @@ def magic(x, y, a):
     if a < 13:
         return
     else:
-        return magic(x, y+a/3, a/3), magic(x+a/3, y, a/3), magic(x+a/3, y+2*a/3, a/3), magic(x+2*a/3, y+a/3, a/3)
+        return magic(x+a/3, y, a/3), magic(x, y+a/3, a/3), magic(x+2*a/3, y+a/3, a/3), magic(x+a/3, y+2*a/3, a/3)
 
+lime_box = canvas.create_rectangle(0, 0, 600, 600, fill='yellow')
 magic(0, 0, 600)
 
 # magic(0, 100, 100)

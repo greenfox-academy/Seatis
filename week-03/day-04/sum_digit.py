05f51d44cb31217b2584b61e6fd25d514f2849be
Seatis <atikezer@gmail.com>
2017-09-21 15:15:10 +0200

    feat(day-04): sum_digit.py is done

---
+++ week-03/day-04/sum_digit.py
@@ -0,0 +1,14 @@
+# Given a non-negative int n, return the sum of its digits recursively (no loops). 
+# Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while 
+# divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+def sum_digits(n):
+  if n <= 0:
+    return 0
+  else:
+    return n % 10 + sum_digits(n // 10)
+
+# def sumdigits(n):
+#     return n and n%10 + sumdigits(n//10)
+
+print(sum_digits(193))

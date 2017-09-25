d21f32f7be7151f6f67a1ae1c76632f6b7a3d736
Seatis <atikezer@gmail.com>
2017-09-25 17:01:40 +0200

    day-01(feat): post_it.py is done

---
+++ week-04/day-01/post_it.py
@@ -0,0 +1,18 @@
+class PostIt:
+    background_color = ""
+    text = ""
+    text_color = ""
+
+text_01 = PostIt()
+text_02 = PostIt()
+text_03 = PostIt()
+
+text_01.background_color = "orange"
+text_01.text = "Idea 1"
+text_01.text_color = "blue"
+text_02.background_color = "pink"
+text_02.text = "Awesome"
+text_02.text_color = "black"
+text_03.background_color = "yellow"
+text_03.text = "Superb!"
+text_03.text_color = "green"

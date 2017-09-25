fb7ff389962888b5f009b088cc231cff676a25ac
Seatis <atikezer@gmail.com>
2017-09-25 17:06:07 +0200

    day-01(feat): double_homework.py is done

---
+++ week-04/day-01/double_homework.py
@@ -0,0 +1,7 @@
+trees = [
+    {"type": "", "leaf_color": "", "age": 0, "sex": ""},
+    {"type": "", "leaf_color": "", "age": 0, "sex": ""},
+    {"type": "", "leaf_color": "", "age": 0, "sex": ""},
+    {"type": "", "leaf_color": "", "age": 0, "sex": ""},
+    {"type": "", "leaf_color": "", "age": 0, "sex": ""}
+]

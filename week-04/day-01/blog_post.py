a0a01f5ab95be5e8b8748b0b09c6e98951e7f34a
Seatis <atikezer@gmail.com>
2017-09-25 17:03:15 +0200

    day-01(feat): bog_post.py is done

---
+++ week-04/day-01/blog_post.py
@@ -0,0 +1,26 @@
+class BlogPost:
+    author_name = ""
+    title = ""
+    text = ""
+    publication_date = ""
+
+blog_post_1 = BlogPost()
+blog_post_2 = BlogPost()
+blog_post_3 = BlogPost()
+
+blog_post_1.author_name = "John Doe"
+blog_post_1.title = "Lorem Ipsum"
+blog_post_1.text = "Lorem ipsum dolor sit amet."
+blog_post_1.publication_date = "2000.05.04"
+
+blog_post_2.author_name = "Tim Urban"
+blog_post_2.title = "Wait but why"
+blog_post_2.text = "A popular long-form, stick-figure-illustrated blog about almost everything."
+blog_post_2.publication_date = "2010.10.10"
+
+blog_post_3.author_name = "William Turton"
+blog_post_3.title = "One Engineer Is Trying to Get IBM to Reckon With Trump"
+blog_post_3.text = "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing."
+blog_post_3.publication_date = "2017.03.28"
+
+

3c5af857285298e02cf0dfbfc828a66bf5e27015
Seatis <atikezer@gmail.com>
2017-09-26 17:07:28 +0200

    feat(day-02): orgnization_new is done.py

---
+++ week-04/day-02/organization_new.py
@@ -0,0 +1,112 @@
+class Person(object):
+    
+    def __init__(self, name = "Jane Doe", age = 30, gender = "female"):
+        self.name = name
+        self.age = age
+        self.gender = gender
+    
+    def introduce(self):
+        print("Hi, I'm " + self.name + ", a " + str(self.age) + " year old " + self.gender + ".")
+    
+    def get_goal(self):
+        print("My goal is: Live for the moment!")
+
+class Student(Person):
+
+    skipped_days = 0
+
+    def __init__(self, name = "Jane Doe", age = 30, gender = "female", previous_organization = "The School of Life"):
+        super().__init__(name, age, gender)
+        self.previous_organization = previous_organization
+    
+    def introduce(self):
+        print("Hi, I'm " + self.name + ", a " + str(self.age) + " year old " + self.gender + " from " + self.previous_organization + " who skipped " + str(self.skipped_days) + " from the course already.")
+    
+    def get_goal(self):
+        print("Be a junior software developer.")
+    
+    def skip_days(self, number_of_days):
+        self.skipped_days += number_of_days
+
+class Mentor(Person):
+    
+    def __init__(self, name = "Jane Doe", age = 30, gender = "female", level = "intermediate"):
+        super().__init__(name, age, gender)
+        self.level = level
+    
+    def get_goal(self):
+        print("Educate brilliant junior software developers.")
+    
+    def introduce(self):
+        print("Hi, I'm " + self.name + ", a " + str(self.age) + " year old " + self.gender + " " + self.level + " mentor.")
+
+class Sponsor(Person):
+
+    hired_students = 0
+
+    def __init__(self, name = "Jane Doe", age = 30, gender = "female", company = "Google"):
+        super().__init__(name, age, gender)
+        self.company = company
+    
+    def introduce(self):
+        print("Hi, I'm " + self.name + ", a " + str(self.age) + " year old " + self.gender + " who represents " + self.company + " and hired " + str(self.hired_students) + " students so far.")
+
+    def hire(self):
+        self.hired_students += 1
+    
+    def get_goal(self):
+        print("Hire brilliant junior software developers.")
+
+class PallidaClass(object):
+    
+    def __init__(self, class_name):
+        self.class_name = class_name
+        self.students = []
+        self.mentors = []
+
+    def add_student(self, Student):
+        self.students.append(Student)
+
+    def add_mentor(self, Mentor):
+        self.mentors.append(Mentor)
+
+    def info(self):
+        print("Pallida " + self.class_name + " class has " + str(len(self.students)) + " and " + str(len(self.mentors)) + " mentors.")
+
+
+people = []
+
+mark = Person('Mark', 46, 'male')
+people.append(mark)
+jane = Person()
+people.append(jane)
+john = Student('John Doe', 20, 'male', 'BME')
+people.append(john)
+student = Student()
+people.append(student)
+gandhi = Mentor('Gandhi', 148, 'male', 'senior')
+people.append(gandhi)
+mentor = Mentor()
+people.append(mentor)
+sponsor = Sponsor()
+people.append(sponsor)
+elon = Sponsor('Elon Musk', 46, 'male', 'SpaceX')
+people.append(elon)
+student.skip_days(3)
+
+for i in range(5):
+    elon.hire()
+
+for i in range(3):
+    sponsor.hire()
+
+for member in people:
+    member.introduce()
+    member.get_goal()
+
+badass = PallidaClass('BADA55')
+badass.add_student(student);
+badass.add_student(john);
+badass.add_mentor(mentor);
+badass.add_mentor(gandhi);
+badass.info();

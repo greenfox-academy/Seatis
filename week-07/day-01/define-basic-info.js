b86185ad6f1511a287d9895fe82b5535fbe20f7c
Seatis <atikezer@gmail.com>
2017-10-23 14:38:02 +0200

    feat(day-01): variable-mutation.js is done

---
+++ week-07/day-01/define-basic-info.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let name = "Attila Kezer"
+let age = 31
+let height = 1.84
+let married = false
+
+console.log("My name is: " + name + ", I am " + age + " years old, my height is: " + height + " m\nI said, I am married, but it is a " + married + " statement!");

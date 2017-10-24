97abfea115b10f287ea9ff5ed96719118201d811
Seatis <atikezer@gmail.com>
2017-10-24 22:37:24 +0200

    feat(day-02): takes-longer.js is done

---
+++ week-07/day-02/strings/takes-longer.js
@@ -0,0 +1,8 @@
+'use strict';
+// When saving this quote a disk error has occured. Please fix it.
+// Add "always takes longer than" to between the words "It" and "you"
+
+var quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law."
+
+quote = quote.slice(0, quote.indexOf("you")) + "always takes longer than" + quote.slice(quote.indexOf("you")-1);
+console.log(quote);

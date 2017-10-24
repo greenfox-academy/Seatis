1fb8abf909f973cd3770e9366d9a2ffd5b1d9a40
Seatis <atikezer@gmail.com>
2017-10-24 13:48:26 +0200

    feat(day-02): append-a-2.js is done

---
+++ week-07/day-02/append-a-2.js
@@ -0,0 +1,12 @@
+'use strict';
+// - Create a variable named `nimals`
+//   with the following content: `["kuty", "macsk", "cic"]`
+// - Add all elements an `"a"` at the end
+// - try to use built in functions instead of loops
+
+var nimals = ["kuty", "macsk", "cic"];
+nimals = nimals.map(function(a) {
+  return a + "a";
+});
+
+console.log(nimals);

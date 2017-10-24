6f50810a467cca098c5db2b33ce2dd1dce2dadcf
Seatis <atikezer@gmail.com>
2017-10-24 22:22:56 +0200

    feat(day-02): url-fixer.js is done

---
+++ week-07/day-02/strings/url-fixer.js
@@ -0,0 +1,10 @@
+'use strict';
+// Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+// Also, the URL is missing a crutial component, find out what it is and insert it too!
+
+var url = "https//www.reddit.com/r/nevertellmethebots";
+var urlArray = url.split("");
+urlArray.splice(5, 0, ":");
+url = urlArray.join("");
+
+console.log(url);

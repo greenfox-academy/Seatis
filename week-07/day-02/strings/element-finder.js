743edf4a66607242228417817ca7fb52ff18b310
Seatis <atikezer@gmail.com>
2017-10-24 23:25:48 +0200

    feat(day-02): element-finder.js is done

---
+++ week-07/day-02/strings/element-finder.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Check if the array contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+
+var numbers = [1,2,3,4,5,6,8];
+
+if (numbers.includes(7)) {
+  console.log("Hoorray");
+} else {
+  console.log("Noooooo");
+}

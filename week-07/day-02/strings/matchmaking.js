c28a90c4785207dcaa95e80b2c71ceb13054adff
Seatis <atikezer@gmail.com>
2017-10-24 23:08:49 +0200

    feat(day-02): matchmaking.js is done

---
+++ week-07/day-02/strings/matchmaking.js
@@ -0,0 +1,14 @@
+'use strict';
+// Join the two array by matching one girl with one boy in the order array
+// Exepected output: ["Eve", "Joe", "Ashley", "Fred"...]
+
+var girls = ["Eve","Ashley","Bözsi","Kat","Jane"];
+var boys = ["Joe","Fred","Béla","Todd","Neef","Jeff"];
+var order = [];
+
+for (let i = 0; i < girls.length; i++) {
+  order.push(girls[i]);
+  order.push(boys[i]);
+}
+
+console.log(order);

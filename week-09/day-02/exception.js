acdf247333d419d4996e7f2063f286a5da4c43fb
Seatis <atikezer@gmail.com>
2017-11-07 14:29:10 +0100

    feat(day-02): exception.js is done

---
+++ week-09/day-02/exception.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// Handle the exceptions in the addString() function. It should check the type of the
+// arguments, throw the right error and write it to the console.
+// It should add the strings too if the arguments are appropriate.
+
+let  addString = function(str1, str2, printStr){
+  if (typeof str1 !== 'string'){
+    throw new Error('"str1" is not a string');
+  } else if (typeof str2 !== 'string'){
+    throw new Error('"str2" is not a string');
+  } else if (typeof printStr !== 'function'){
+    throw new Error('"printStr" is not a function');
+  }
+  let newStr = str1 + str2;
+  printStr(newStr);
+}
+
+let printStr = function(str) {
+  console.log(str);
+}
+
+try {
+	addString('hello', 'bello', printStr);
+} catch (err) {
+	console.log('catching error:');
+	console.log(err.message);
+}

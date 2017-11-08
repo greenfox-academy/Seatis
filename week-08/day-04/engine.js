99c4570db4f6f136851230f06e96207307027ffe
Seatis <atikezer@gmail.com>
2017-11-09 00:51:18 +0100

    fix(day-04): frontend project has been updated

--- week-08/day-04/engine.js
+++ week-08/day-04/engine.js
@@ -1,80 +1,68 @@
-var mainSection = document.querySelector('section.main');
+var myRequest = new XMLHttpRequest();
+myRequest.open("GET", 'http://secure-reddit.herokuapp.com/simple/posts', false);
+myRequest.send(null);
 
-data.forEach(function(element) {
-  var myIndex = document.createElement('div');
-  myIndex.classList.add('index');
-  myIndex.textContent = element.id;
-  var myArrows = document.createElement('div');
-  myArrows.classList.add('arrows');
-  var myTitle = document.createElement('div');
-  myTitle.classList.add('title');
-  var upVoteDiv = document.createElement('div');
-  var upVoteImg = document.createElement('img');
-  upVoteImg.setAttribute('src', 'assets/upvote.png');
-  var downVoteDiv = document.createElement('div');
-  var downVoteImg = document.createElement('img');
-  downVoteImg.setAttribute('src', 'assets/downvote.png');
-  var voteDiv = document.createElement('div');
-  voteDiv.textContent = element.vote;
-  var myHead = document.createElement('div');
-  myHead.classList.add('head');
-  myHead.textContent = element.title;
-  var myDescription = document.createElement('div');
-  myDescription.classList.add('description');
-  myDescription.textContent = "submitted " + element.submitted + " month ago by " + element.name;
-  var myRemove = document.createElement('div');
-  myRemove.classList.add('remove');
-  myRemove.textContent = "modify remove";
 
-  upVoteDiv.appendChild(upVoteImg);
-  downVoteDiv.appendChild(downVoteImg);
-  myArrows.appendChild(upVoteDiv);
-  myArrows.appendChild(voteDiv);
-  myArrows.appendChild(downVoteDiv);
-  myTitle.appendChild(myHead);
-  myTitle.appendChild(myDescription);
-  myTitle.appendChild(myRemove);
-  mainSection.appendChild(myIndex);
-  mainSection.appendChild(myArrows);
-  mainSection.appendChild(myTitle);
+if (myRequest.readyState === XMLHttpRequest.DONE) {
+console.log("Everything is good, the response was received.");
+} else {
+  console.log("Not ready yet.");
+}
+console.log(myRequest.readyState);
+
+var requestAPI = JSON.parse(myRequest.response);
+console.log(requestAPI.posts[0]);
+
+if (document.querySelector('input') != null) {
+  var myURL = document.querySelector('input');
+  var myTitle = document.querySelector('textarea');
+  var myButton = document.querySelector('div.submit')
+  myButton.addEventListener('click', function() {
+    var body = JSON.stringify({
+      "title": myTitle.value,
+      "url": myURL.value
+    });
+    
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        document.querySelector('p.response').textContent = 'The post has been sent!';
+        myTitle.value = '';
+        myURL.value = '';
+      }
+    });
+    
+    xhr.open("POST", "http://secure-reddit.herokuapp.com/simple/posts");
+    xhr.setRequestHeader("accept", "application/json");
+    xhr.setRequestHeader("content-type", "application/json");
+
+    xhr.send(body);
+  });
+}
+
+var mainSection = document.querySelector('section.main-container');
+
+requestAPI.posts.forEach(function(element) {
+  if (element.user === null) {
+    var currentUser = 'Anonymus';
+  } else {
+    var currentUser = element.user;
+  }
+  const markup = `
+  <div class="index">${element.id}</div>
+  <div class="arrows">
+    <div><img id="up" src="assets/upvote.png" alt=""></div>
+    <div id="vote">${element.score}</div>
+    <div><img id="down" src="assets/downvote.png" alt=""></div>
+  </div>
+  <div class="title">
+    <div class="head">${element.title}</div>
+    <div class="description">submitted ${element.timestamp} month ago by ${currentUser}</div>
+    <div class="remove">modify remove</div>
+  </div>
+  `
+  mainSection.innerHTML += markup;
 
 }, this);
-
-
-// myButton.addEventListener('click', function() {
-//   var url = "https://devru-latitude-longitude-find-v1.p.mashape.com/latlon.php?location="
-//   var city = myInput.value;
-//   var citySplit = city.split(" ");
-//   var cityJoin = citySplit.join('+');
-//   url += cityJoin;
-//   console.log(url);
-
-//   var myRequest = new XMLHttpRequest();
-//   myRequest.open("GET", url, false);
-//   myRequest.setRequestHeader("X-Mashape-Key", "kjALftGIZ6mshmtVHprgyTTaxuRPp1u83gpjsnlaeZCHN1kTUM");
-//   myRequest.setRequestHeader("Accept", "application/json");
-//   myRequest.send(null);
-
-
-//   if (myRequest.readyState === XMLHttpRequest.DONE) {
-//   console.log("Everything is good, the response was received.");
-//   } else {
-//     console.log("Not ready yet.");
-//   }
-//   console.log(myRequest.readyState);
-
-  
-//   var requestAPI = JSON.parse(myRequest.response);
-//   console.log(requestAPI);
-//   latCoor = requestAPI.Results[0].lat;
-//   lonCoor = requestAPI.Results[0].lon;
-//   console.log(latCoor);
-//   console.log(lonCoor);
-//   var myPs = document.querySelectorAll('p');
-//   myPs[0].textContent = "Latitude: " + latCoor;
-//   myPs[1].textContent = "Longitude: " + lonCoor;
-  
-//   var mapURL = "https://www.google.com/maps/embed/v1/place?key=AIzaSyCr5KoGCCUQ6-XzyRpmActyVjzYvXbB_Z4&q=Space+Needle,Seattle+WA"
-//   var currentMapURL = mapURL += "&center=" + latCoor + "," + lonCoor;
-//   currentMap.setAttribute('src', currentMapURL);
-// }); 

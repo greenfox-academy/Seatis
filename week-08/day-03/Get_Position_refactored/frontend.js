da1b9ba22cd41b2051543a04a16f452f8cc238e0
Seatis <atikezer@gmail.com>
2018-01-20 00:37:08 +0100

    feat(frontend.js): frontend get the coordinates from the api

--- week-08/day-03/Get_Position_refactored/frontend.js
+++ week-08/day-03/Get_Position_refactored/frontend.js
@@ -11,3 +11,26 @@ function ajax (method, url, data, callback) {
   xhr.setRequestHeader("Accept", "application/json");
   xhr.send(data);
 }
+
+function updateCoordinates(result) {
+  let myCoordinates = document.querySelector('section.coordinates');
+  const markup = `<p>The city: ${result.Results[0].name}</p>
+                  <p>Latitude: ${result.Results[0].lat}</p>
+                  <p>Longitude: ${result.Results[0].lon}</p>`;
+  myCoordinates.innerHTML = markup;
+  console.log(result);
+}
+
+function core() {
+  let myButton = document.querySelector('button');
+  let myInput = document.querySelector('input');
+  let currentMap = document.querySelector('iframe');
+  myButton.addEventListener('click', function() {
+    let url = "https://devru-latitude-longitude-find-v1.p.mashape.com/latlon.php?location="
+    url += myInput.value.replace(" ", "+");
+    console.log(url);
+    ajax('GET', url, null, updateCoordinates);
+  });
+}
+
+core();

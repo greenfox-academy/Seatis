fcfd624f8042e8801804f7da309a12566a08b26f
Seatis <atikezer@gmail.com>
2018-01-22 15:17:22 +0100

    feat(anagram-test): more tests have been added

--- week-09/day-02/anagram-test.js
+++ week-09/day-02/anagram-test.js
@@ -4,9 +4,45 @@ var test = require('tape');
 var anagram = require('./anagram.js');
 
 
-test('anagram test', function (t) {
+test('anagram ok test', function (t) {
   var actual = anagram.isAnagram('votes', 'stove');
-  var expected = 'apple';
+  var expected = true;
   t.equal(actual, expected);
   t.end();
 });
+
+test('anagram fail test with same length', function (t) {
+  var actual = anagram.isAnagram('votes', 'stive');
+  var expected = false;
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('anagram fail test with different length', function (t) {
+  var actual = anagram.isAnagram('votes', 'stover');
+  var expected = false;
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('anagram fail test with missing parameter', function (t) {
+  var actual = anagram.isAnagram('votes');
+  var expected = false;
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('anagram fail test with missing parameters', function (t) {
+  var actual = anagram.isAnagram();
+  var expected = false;
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('anagram fail test with number and string', function (t) {
+  var actual = anagram.isAnagram('hello', 6);
+  var expected = false;
+  t.equal(actual, expected);
+  t.end();
+});
+
--- week-09/day-02/anagram.js
+++ week-09/day-02/anagram.js
@@ -1,9 +1,11 @@
 var anagram = {
   isAnagram: function(string_1, string_2) {
-    if (string_1.length === string_2.length) {
+    if (string_1 && string_2 && string_1.length === string_2.length) {
       let array_1 = string_1.split('').sort();
       let array_2 = string_2.split('').sort();
       return array_1.filter((letter, i) => letter === array_2[i]).length === array_1.length;
+    } else {
+      return false;
     }
   }
 }

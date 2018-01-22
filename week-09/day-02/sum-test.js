9fc2fd4a2c7f0478802f273df7757157443b9b83
Seatis <atikezer@gmail.com>
2018-01-22 14:35:51 +0100

    fix(apple-test, sum-test): more tests has been added

--- week-09/day-02/sum-test.js
+++ week-09/day-02/sum-test.js
@@ -4,9 +4,37 @@ var test = require('tape');
 var sum = require('./sum.js');
 
 
+test('array test', function (t) {
+  var actual = sum.sumElements([6, 2, 4]);
+  var expected = 12;
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('one element test', function (t) {
+  var actual = sum.sumElements([2]);
+  var expected = 2;
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('empty array', function (t) {
+  var actual = sum.sumElements([]);
+  var expected = 0;
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('null test', function (t) {
+  var actual = sum.sumElements(null);
+  var expected = "Null is not an array!";
+  t.equal(actual, expected);
+  t.end();
+});
+
 test('string test', function (t) {
-  var actual = sum.sumElements([6]);
-  var expected = 6;
+  var actual = sum.sumElements('string');
+  var expected = "This is a string!";
   t.equal(actual, expected);
   t.end();
 });
--- week-09/day-02/sum.js
+++ week-09/day-02/sum.js
@@ -1,10 +1,16 @@
 var sum = {
   sumElements: function(numList) {
     var summa = 0;
-    numList.forEach(function(element) {
-      summa += element;
-    }, this);
-    return summa;
+    if (numList === null) {
+      return "Null is not an array!";
+    } else if (typeof numList === 'string') {
+      return "This is a string!";
+    } else {
+      numList.forEach(function(element) {
+        summa += element;
+      }, this);
+      return summa;
+    }
   }
 }
 

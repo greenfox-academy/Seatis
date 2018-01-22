b46788e73fbb06a9a9c7ded10d9f559d9cdfd405
Seatis <atikezer@gmail.com>
2018-01-22 15:11:59 +0100

    feat(anagram-test): first test has been fixed

--- week-09/day-02/anagram.js
+++ week-09/day-02/anagram.js
@@ -1,9 +1,11 @@
 var anagram = {
   isAnagram: function(string_1, string_2) {
-    
+    if (string_1.length === string_2.length) {
+      let array_1 = string_1.split('').sort();
+      let array_2 = string_2.split('').sort();
+      return array_1.filter((letter, i) => letter === array_2[i]).length === array_1.length;
+    }
   }
 }
-console.log('hello'.split('').sort());
-console.log('leolh'.split('').sort() == 'hello'.split('').sort());
 
 module.exports = anagram;

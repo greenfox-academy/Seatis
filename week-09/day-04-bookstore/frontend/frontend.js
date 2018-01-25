2ba627dc452c45bda1fbe3513e4a70bba1bda71f
Seatis <atikezer@gmail.com>
2018-01-25 12:41:54 +0100

    feat(bookstore-refactored): ajax has been created on frontend

---
+++ week-09/day-04-bookstore/frontend/frontend.js
@@ -0,0 +1,15 @@
+"use strict"
+
+function ajax (method, url, data, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+      var requestAPI = JSON.parse(xhr.response);
+      callback(requestAPI);
+    }
+  });
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(data);
+}

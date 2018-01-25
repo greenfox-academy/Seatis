3eae67a9b78454c1e6dac26598b522cfab88aaff
Seatis <atikezer@gmail.com>
2018-01-25 12:53:37 +0100

    fix(bookstore-refactored): backend variable-declarations has been fixed

--- week-09/day-04-bookstore/backend.js
+++ week-09/day-04-bookstore/backend.js
@@ -28,13 +28,13 @@ app.get('/', function(req, res) {
 app.get('/books', function(req, res) {
   let data = [];
   if (req.query.category) {
-    let queryString = `SELECT book_name, aut_name, cate_descrip, pub_name, book_price
+    var queryString = `SELECT book_name, aut_name, cate_descrip, pub_name, book_price
     FROM book_mast INNER JOIN author ON book_mast.aut_id=author.aut_id 
     INNER JOIN category ON book_mast.cate_id=category.cate_id 
     INNER JOIN publisher ON book_mast.pub_id=publisher.pub_id 
     WHERE cate_descrip='${req.query.category}'`;
   } else {
-    let queryString = `SELECT book_name, aut_name, cate_descrip, pub_name, book_price
+    var queryString = `SELECT book_name, aut_name, cate_descrip, pub_name, book_price
                      FROM book_mast INNER JOIN author ON book_mast.aut_id=author.aut_id 
                      INNER JOIN category ON book_mast.cate_id=category.cate_id 
                      INNER JOIN publisher ON book_mast.pub_id=publisher.pub_id`;

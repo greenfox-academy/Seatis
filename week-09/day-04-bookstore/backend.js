ab2050e350d03e0cffa3f322d3095193ab88bd08
Seatis <atikezer@gmail.com>
2018-01-25 12:38:35 +0100

    feat(bookstore-refactored): /get endpoint is done

--- week-09/day-04-bookstore/backend.js
+++ week-09/day-04-bookstore/backend.js
@@ -24,3 +24,25 @@ connection.connect();
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/frontend/index.html');
 });
+
+app.get('/books', function(req, res) {
+  var data = [];
+  if (req.query.category) {
+    var queryString = `SELECT book_name, aut_name, cate_descrip, pub_name, book_price
+    FROM book_mast INNER JOIN author ON book_mast.aut_id=author.aut_id 
+    INNER JOIN category ON book_mast.cate_id=category.cate_id 
+    INNER JOIN publisher ON book_mast.pub_id=publisher.pub_id 
+    WHERE cate_descrip='${req.query.category}'`;
+  } else {
+  var queryString = `SELECT book_name, aut_name, cate_descrip, pub_name, book_price
+                     FROM book_mast INNER JOIN author ON book_mast.aut_id=author.aut_id 
+                     INNER JOIN category ON book_mast.cate_id=category.cate_id 
+                     INNER JOIN publisher ON book_mast.pub_id=publisher.pub_id`;
+  }
+  connection.query(queryString, function(err, result, fileds) {
+      result.forEach(function(element){
+        data.push({'title': element.book_name, 'author': element.aut_name, 'category': element.cate_descrip, 'publisher': element.pub_name, 'price': element.book_price})
+      });
+      res.send({'books': data});
+  });
+});

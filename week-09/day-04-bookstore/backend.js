ca68b7aa74684837e006355af0bfc962a9ab46fc
Seatis <atikezer@gmail.com>
2018-01-25 12:36:41 +0100

    feat(bookstore-refactored): express, cors has been loaded and mysql connected

---
+++ week-09/day-04-bookstore/backend.js
@@ -0,0 +1,22 @@
+'use strict'
+
+var express = require('express');
+var cors = require('cors');
+var mysql = require('mysql');
+
+var app = express();
+
+app.use(express.json());
+
+app.use(express.static('./frontend'));
+app.use('/assets', express.static('./assets'));
+app.use(cors());
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'GR18pv',
+  database: 'bookstore'
+});
+
+connection.connect();

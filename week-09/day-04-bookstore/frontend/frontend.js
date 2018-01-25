0dde1612c546c360ff60b5975bff756b3f0bf924
Seatis <atikezer@gmail.com>
2018-01-25 12:42:34 +0100

    feat(bookstore-refactored): callback (appendTable) has been created

--- week-09/day-04-bookstore/frontend/frontend.js
+++ week-09/day-04-bookstore/frontend/frontend.js
@@ -13,3 +13,26 @@ function ajax (method, url, data, callback) {
   xhr.setRequestHeader("Content-Type", "application/json");
   xhr.send(data);
 }
+
+function appendTable(result){
+  var table = document.querySelector('section table');
+  table.innerHTML = `<tr>
+                      <th>Book</th>
+                      <th>Athor</th> 
+                      <th>Category</th>
+                      <th>Publisher</th>
+                      <th>Price</th>
+                    </tr>
+  `
+  result.books.forEach(function(element) {
+    const markup = `<tr>
+                      <td>${element.title}</td>
+                      <td>${element.author}</td>
+                      <td>${element.category}</td>
+                      <td>${element.publisher}</td>
+                      <td>${element.price}</td>
+                    </tr>
+    `
+    table.innerHTML += markup;
+  }, this);
+}

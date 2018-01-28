c24538189bc0d9fffda7a5c189d4aea6da72ec5f
Seatis <atikezer@gmail.com>
2018-01-28 13:01:56 +0100

    feat(reddit-fullstack): mysql connenction is done

--- week-09/day-05-reddit-fullstack/backend.js
+++ week-09/day-05-reddit-fullstack/backend.js
@@ -10,8 +10,25 @@ app.use(express.static('./frontend'));
 app.use('/assets', express.static('./assets'));
 app.use(cors());
 
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'GR18pv',
+  database: 'reddit'
+});
+
+connection.connect(function(err) {
+  if(err){
+    console.log("Error connecting to Db!");
+    return;
+  }
+  console.log("Connection established.");
+});
+
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+
+conn.end();
 app.listen(5000, () => console.log('Running'));

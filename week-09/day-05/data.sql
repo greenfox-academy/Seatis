90f83d3c1b0a1edea515022a544f7c2c5f3ce2be
Seatis <atikezer@gmail.com>
2018-01-28 12:49:06 +0100

    feat(reddit-fullstack): clean up

--- week-09/day-05/data.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE posts (
-  id      INT PRIMARY KEY AUTO_INCREMENT,
-  title   VARCHAR(150) NOT NULL,
-  url     VARCHAR(150) NOT NULL,
-  score   INT          NOT NULL,
-  timest  INT          NOT NULL
-);

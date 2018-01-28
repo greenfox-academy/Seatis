117b903c51df5fcf3b496a5843436b87e66ab618
Seatis <atikezer@gmail.com>
2018-01-28 14:36:29 +0100

    feat(reddit-fullstack): post endpoint has been created

--- week-09/day-05-reddit-fullstack/backend.js
+++ week-09/day-05-reddit-fullstack/backend.js
@@ -33,8 +33,7 @@ app.get('/posts', function(request, response) {
   let data = [];
   connection.query('SELECT * from posts', function(error, result) {
     if (error) {
-      console.log(error.toString());
-      response.status(500).send('Database error');
+      response.status(500).send(`Database error: ${error.toString()}`);
       return;
     }
     result.forEach(function(row){
@@ -42,6 +41,32 @@ app.get('/posts', function(request, response) {
     });
     response.send({'posts': data});
   });
+});
+
+app.post('/posts', function (request, response) {
+  var queryString = `INSERT INTO posts (title, url, timestamp) VALUES ('${request.body.title}', '${request.body.url}', ${Date.now()})`;
+  var queryCheck = `SELECT * from posts WHERE title='${request.body.title}'`;
+  connection.query(queryString, function(error, result) {
+    if (error) {
+      response.status(500).send(`Database error: ${error.toString()}`);
+      return;
+    }
+  });
+  connection.query(queryCheck, function(error, result) {
+    if (error) {
+      response.status(500).send(`Database error: ${error.toString()}`);
+      return;
+    }
+    response.send({
+      "id": result[0].id,      
+      "title": result[0].title,
+      "url": result[0].url,
+      "timestamp": result[0].timestamp,
+      "score": result[0].score, 
+      "owner": result[0].owner
+    });
+  });
+  
 });
 
 // connection.end();
